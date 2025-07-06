fe5c76a318ef86eb2393b2eb69a836c91007a3d9
hexHenrique <henriquepiresh3@gmail.com>
2025-07-05 22:47:03 -0300

    feat(dadosmocados): adição de dados mocados no modelo da API do FireBase

--- src/pages/psychologist/Appintments.tsx
+++ src/pages/psychologist/Appintments.tsx
@@ -1,7 +1,7 @@
 // src/pages/psychologist/Appintment.tsx
 
-import PsychologistLayout from '../../layouts/PsychologistLayout';
-import AppointmentDetailsPage from '../../festures/psychologist/appointments/AppointmentDetailsPage';
+import PsychologistLayout from "../../layouts/PsychologistLayout";
+import AppointmentDetailsPage from "../../features/psychologist/appointments/AppointmentDetailsPage";
 
 export default function Home() {
   return (
